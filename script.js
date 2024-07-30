d82f9e55d6302cc426c46cfcec37a3d4ed2f70d7
Unsmanuel25 <priscomanuel479@gmail.com>
2024-07-30 18:06:59 +0200

    steps 2

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ console.log('JS OK');
 
 /*
 1-recupero il/gli elementi della pagina
-1-chiedere nome utente
-2-chiedere cognome utente
-3-chiedere colore preferito utente
+2-chiedere nome utente
+3-chiedere cognome utente
+4-chiedere colore preferito utente
+5-calcolo informazioni
+6-stampo il messaggio in pagina
 */
 
